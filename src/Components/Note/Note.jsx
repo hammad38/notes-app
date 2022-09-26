511f38377547ca6b86909d81f724c929e0e59f2d
hammad38 <hammadsami119@gmail.com>
2022-09-26 23:57:49 +0500

    .

--- src/Components/Note/Note.jsx
+++ src/Components/Note/Note.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
+import "./Note.css"
 
 const Note = () => {
   return (
-    <div className='note'>
-    <textarea  className="note_text">
-    </textarea>  
+    <div className="note">
+      <textarea className="note_text" />
       <p>11:45 PM 26 September</p>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Note
+export default Note;
