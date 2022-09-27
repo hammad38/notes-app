e01565d1b34e8f4fd867b81a7c1223a7f637138d
hammad38 <hammadsami119@gmail.com>
2022-09-27 16:43:48 +0500

    .

--- src/Components/Note/Note.jsx
+++ src/Components/Note/Note.jsx
@@ -4,7 +4,7 @@ import "./Note.css"
 const Note = () => {
   return (
     <div className="note">
-      <textarea className="note_text" />
+    <textarea className="note_text" />
       <p>11:45 PM 26 September</p>
     </div>
   );
