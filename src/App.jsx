789d843b5fcbdedb8c30e81b4e95af77065809b3
hammad38 <hammadsami119@gmail.com>
2022-09-28 20:07:23 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,27 @@ import Sidebar from "./Components/Sidebar/Sidebar";
 import "./App.css";
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      text: "Write something",
-      time: "12:00 PM",
-      color: "yellow",
-    },
-    {
-      text: "sadf",
-      time: "21:00 AM",
-      color: "pink",
-    },
-    {
-      text: ";lkjafd",
-      time: "23:00 AM",
-      color: "lightgray",
-    },
-    {
-      text: "Write something",
-      time: "12:00 AM",
-      color: "purple",
-    },
+  const [notes, setNotes] = useState([''
+    // {
+    //   text: "Write something",
+    //   time: "12:00 PM",
+    //   color: "yellow",
+    // },
+    // {
+    //   text: "sadf",
+    //   time: "21:00 AM",
+    //   color: "pink",
+    // },
+    // {
+    //   text: ";lkjafd",
+    //   time: "23:00 AM",
+    //   color: "lightgray",
+    // },
+    // {
+    //   text: "Write something",
+    //   time: "12:00 AM",
+    //   color: "purple",
+    // },
   ]);
   const addNote = (color) => {
     const tempNotes = [...notes];
