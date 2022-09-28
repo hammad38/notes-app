f94de52aac9657d74d02362bd4aa5c22f8e420c7
hammad38 <hammadsami119@gmail.com>
2022-09-28 16:31:15 +0500

    .

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -6,18 +6,17 @@ import "./Sidebar.css";
 const Sidebar = () => {
   const colors = ["#fe9b72", "#fec971", "#00d4fe", "#b693fd", "#e4ee91"];
 
+  
   return (
     <div className="sidebar">
-      <HiPlusCircle className="plusicon" />
+      <HiPlusCircle className="plusicon"  />
       <ul className="sidebar_list">
-
         {colors.map((color, index) => (
           <li
-          key={index}
+            key={index}
             className="sidebar_list_item"
             style={{ backgroundColor: color }}
           ></li>
-          
         ))}
       </ul>
     </div>
