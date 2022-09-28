7cf1e3d435d18719c5e9a77231389f6723f646eb
hammad38 <hammadsami119@gmail.com>
2022-09-28 20:01:42 +0500

    .

--- src/Components/NoteContainer/NoteContainer.jsx
+++ src/Components/NoteContainer/NoteContainer.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Note from "../Note/Note";
 import "./NoteContainer.css"
+
 const NoteContainer = (props) => {
+
   return (
+    
     <div className="note-container">
     <h2>Notes</h2>  
     <div className="note-container_notes custom-scroll">
