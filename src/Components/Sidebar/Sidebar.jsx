2b06f2908fa7ffc5c909d2f7bd4ea59fae18f336
hammad38 <hammadsami119@gmail.com>
2022-09-28 16:14:25 +0500

    add map method

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,11 +4,19 @@ import { HiPlusCircle } from "react-icons/hi";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  const colors = ["#fe9b72", "#fec971", "#00d4fe", "#b693fd", "#e4ee91"];
+
   return (
     <div className="sidebar">
       <HiPlusCircle className="plusicon" />
       <ul className="sidebar_list">
-        <li className="sidebar_list_item"></li>
+        {colors.map((color, index) => (
+          <li
+          key={index}
+            className="sidebar_list_item"
+            style={{ backgroundColor: color }}
+          ></li>
+        ))}
       </ul>
     </div>
   );
