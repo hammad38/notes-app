909f855a15f7166cf467e271541ea2c202f9cdf8
hammad38 <hammadsami119@gmail.com>
2022-09-29 16:46:14 +0500

    .

--- src/Components/Note/Note.jsx
+++ src/Components/Note/Note.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import {AiFillDelete} from "react-icons/ai";
 import "./Note.css";
 
 const Note = (props) => {
   return (
     <div className="note" style={{ backgroundColor: props.note.color }}>
       <textarea className="note_text" defaultValue={props.note.text} />
+      <div className="note_footer">
       <p>{props.note.time}</p>
+      <AiFillDelete />
+      </div>
     </div>
   );
 };
