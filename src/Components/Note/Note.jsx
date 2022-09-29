8bcebe2b0f6abfe40dd3f9a91d740d784358a344
hammad38 <hammadsami119@gmail.com>
2022-09-29 20:58:26 +0500

    .

--- src/Components/Note/Note.jsx
+++ src/Components/Note/Note.jsx
@@ -1,28 +1,53 @@
 import React from "react";
-import {RiDeleteBin6Fill} from "react-icons/ri";
+import { MdDeleteOutline } from "react-icons/md";
 import "./Note.css";
 
 const Note = (props) => {
-  
   //formatting date
-  const formatDate = (value) =>{
-    if(!value) return ""
+  const formatDate = (value) => {
+    if (!value) return "";
 
-    const date = new Date(value)
+    const date = new Date(value);
+    const month = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
 
-    let hrs = date.getHours()
-    let amPm = hrs>12?"AM":"PM"
-    hrs = hrs? hrs : "12" //this is for false value becasue js not accepts zero
-    hrs=hrs>12?hrs=24-hrs:hrs
+    let hrs = date.getHours();
+    let amPm = hrs > 12 ? "PM" : "AM";
+    hrs = hrs > 12 ? (hrs = hrs-12) : hrs;
+    hrs = hrs ? hrs : "12"; //this is for false value becasue js not accepts zero
+    hrs = hrs < 10 ? "0" + hrs : hrs;
 
-  }
+    let min = date.getMinutes();
+    min = min < 10 ? "0" + min : min;
+
+    let day = date.getDate();
+
+    let months = month[date.getMonth()];
+
+    return `${hrs}:${min} ${amPm} ${day} ${months}`;
+  };
 
   return (
     <div className="note" style={{ backgroundColor: props.note.color }}>
       <textarea className="note_text" defaultValue={props.note.text} />
       <div className="note_footer">
-      <p>{props.note.time}</p>
-      <RiDeleteBin6Fill className="deleteIcon" onClick={()=>props.deleteNote(props.note.id)}/>
+        <p>{formatDate(props.note.time)}</p>
+        <MdDeleteOutline
+          className="deleteIcon"
+          onClick={() => props.deleteNote(props.note.id)}
+        />
       </div>
     </div>
   );
